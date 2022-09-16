664235ddc6b9a90e63d5c031b3a2884d273d9318
Ron <wondelrosario@gmail.com>
2022-09-17 05:11:30 +1400

    Add files via upload

---
+++ Task3/ArithmethicTable.java
@@ -0,0 +1,57 @@
+/* 
+ * Code created by: Pagilagan, Marc Owen B.
+ */
+package Task3;
+
+public class ArithmethicTable {
+    public static void main(String[] args) {
+        int varA = 1;
+        int varB = 2;
+        int varC = 3;
+        int varD = 4;
+        int varE = 5;
+        int varF = 6;
+        int varG = 7;
+        int varH = 8;
+        int varI = 9;
+        int varJ = 10;
+        
+        System.out.println("***************************************");
+        System.out.println("M U L T I P L I C A T I O N  T A B L E");
+        System.out.println( "  1 * 1 = " + varA * varA);    
+        System.out.println( "  2 * 2 = " + varB * varB);    
+        System.out.println( "  3 * 3 = " + varC * varC);    
+        System.out.println( "  4 * 4 = " + varD * varD);    
+        System.out.println( "  5 * 5 = " + varE * varE);    
+        System.out.println( "  6 * 6 = " + varF * varF);    
+        System.out.println( "  7 * 7 = " + varG * varG);    
+        System.out.println( "  8 * 8 = " + varH * varH);    
+        System.out.println( "  9 * 9 = " + varI * varI);    
+        System.out.println( "  10 * 10 = " + varJ * varJ); 
+        System.out.println("***************************************");
+        System.out.println("D I V I S I O N  T A B L E");
+        System.out.println( "  1 / 1 = " + varA / varA);    
+        System.out.println( "  2 / 2 = " + varB / varB);    
+        System.out.println( "  3 / 3 = " + varC / varC);    
+        System.out.println( "  4 / 4 = " + varD / varD);    
+        System.out.println( "  5 / 5 = " + varE / varE);    
+        System.out.println( "  6 / 6 = " + varF / varF);    
+        System.out.println( "  7 / 7 = " + varG / varG);    
+        System.out.println( "  8 / 8 = " + varH / varH);    
+        System.out.println( "  9 / 9 = " + varI / varI);    
+        System.out.println( "  10 / 10 = " + varJ / varJ); 
+        System.out.println("***************************************");
+        System.out.println("M O D U L U S  T A B L E");
+        System.out.println( "  1 % 10 = " + varA % varJ);    
+        System.out.println( "  2 % 9 = " + varB % varI);    
+        System.out.println( "  3 % 8 = " + varC % varH);    
+        System.out.println( "  4 % 7 = " + varD % varG);    
+        System.out.println( "  5 % 6 = " + varE % varF);    
+        System.out.println( "  6 % 5 = " + varF % varE);    
+        System.out.println( "  7 % 4 = " + varG % varD);    
+        System.out.println( "  8 % 3 = " + varH % varC);    
+        System.out.println( "  9 % 2 = " + varI % varB);    
+        System.out.println( "  10 % 1 = " + varJ % varA); 
+    }
+  
+}
